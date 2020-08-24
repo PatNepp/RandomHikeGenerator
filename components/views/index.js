501d535cfb8cdd78ffbf8a369c676c36acc52d23
Patrick Nepp <ptnepp@gmail.com>
2020-08-24 16:24:53 -0500

    File name changes for Netlify

--- components/views/index.js
+++ components/views/index.js
@@ -2,5 +2,5 @@ export { default as Contact } from "./Contact";
 export { default as Hike } from "./Hike";
 export { default as Home } from "./Home";
 export { default as Login } from "./LogIn";
-export { default as Signup } from "./Signup";
+export { default as Signup } from "./SignUp";
 export { default as Profile } from "./Profile";
